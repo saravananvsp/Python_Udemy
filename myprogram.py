23bb8f7381b60b72bc6f99c84f8fc9cd676aa309
Saravanan Paramasivan <saravananvsp@gmail.com>
2017-12-07 15:34:09 +0530

    Operatots,list,slicing & dictionary

--- myprogram.py
+++ myprogram.py
@@ -1,2 +1,58 @@
 print("Hello Saravanan")
 print(108)
+
+abc=2323
+print(abc)
+
+abc='a'
+print(abc)
+
+abc="hello"
+print(abc)
+
+abc=22.00
+print(abc)
+
+abc=0x333
+print(abc)
+
+
+abc = input("Enter anything: ")
+print(type(abc))
+print(abc)
+
+abc = input("Enter value: ")
+abc = int(abc)
+print(type(abc))
+print(abc)
+
+pow_abc=3**2
+print(pow_abc)
+
+Str='Sam is here'
+print(Str.upper())
+
+print(Str[0])
+print(Str[:3])
+print(Str[7:])
+print(Str[-3:][-1])
+
+
+List = [122,23.44,'232',"asdasda"]
+print(List)
+List.append("Test")
+print(List)
+List.remove("Test")
+print(List)
+print(List[:2])
+
+Tuples = ('sa',12,2.5)
+print(Tuples)
+
+List_Tuples = [(1,2,'s'),(3.3,3.3,'sss'),(1,4,6)]
+print(List_Tuples)
+print(List_Tuples[2])
+
+Dictionary = {"Name":"sam","city":("chn","bang","mum","dlh")}
+print(Dictionary)
+print(Dictionary["city"][1])
