291ace6a64e941a397dd2f8ec7ae64abd1fc8093
Saravanan Paramasivan <saravananvsp@gmail.com>
2017-12-12 11:44:37 +0530

    myfunction.py exercise update

--- myfunction.py
+++ myfunction.py
@@ -12,16 +12,6 @@ print(str(minutes)+" minutes = "+str(min_to_hours(minutes))+" hours")
 seconds = int(input("Enter Seconds: "))
 print(str(seconds)+" seconds = "+str(sec_to_hours(seconds))+" hours")
 
-def celsius_to_fahrenheit(celsius):
-    if celsius < -273.15:
-        return "Please enter a valid temperature which a physical matter can reach :p"
-    else:
-        fahrenheit = (celsius * 9/5.0)+32
-        return fahrenheit
-
-celsius = float(input("Enter Temperature in Celsius : "))
-print("Temperature in Fahrenheit : "+str(celsius_to_fahrenheit(celsius)))
-
 def string_length(string):
     if type(string) == int:
         return "Sorry, integers don't have length"
@@ -36,3 +26,18 @@ string=44
 print(string_length(string))
 string=44.00
 print(string_length(string))
+
+def celsius_to_fahrenheit(celsius):
+    if celsius < -273.15:
+        return "Please enter a valid temperature which a physical matter can reach :p"
+    else:
+        fahrenheit = (celsius * 9/5.0)+32
+        return fahrenheit
+
+celsius = float(input("Enter Temperature in Celsius : "))
+print("Temperature in Fahrenheit : "+str(celsius_to_fahrenheit(celsius)))
+
+temperatures = [10,-20,-289,100]
+
+for temperature in temperatures :
+    print(celsius_to_fahrenheit(temperature))
